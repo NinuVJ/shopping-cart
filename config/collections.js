7480bd54c88b4d6a62b60f5bbe83bdca8a214c44
NinuVJ <ninujohnson247@gmail.com>
2022-12-21 23:50:11 +0530

    first commit

---
+++ config/collections.js
@@ -0,0 +1,7 @@
+module.exports={
+    USER_COLLECTION:'users',
+    PRODUCT_COLLECTION:'product',
+    CART_COLLECTION:'cart',
+    ORDER_COLLECTION:'order'
+    
+    }
---
+++ helpers/product-helpers.js
@@ -0,0 +1,57 @@
+var db=require('../config/connection')
+var collection=require('../config/collections');
+const { ObjectId } = require('mongodb');
+const { response } = require('express');
+var objectId=require('mongodb').ObjectId
+module.exports={
+
+    addProduct:async(product,callback)=>{
+        console.log(product);
+
+        db.get().collection('product').insertOne(product).then((data)=>{
+            console.log(data);
+            callback(data.insertedId)
+
+        })
+
+    },
+    getAllProducts:()=>{
+        return new Promise(async(resolve,reject)=>{
+                    
+
+            let products=await db.get().collection(collection.PRODUCT_COLLECTION).find().toArray()
+            resolve(products)
+        })
+    },
+    deleteProduct:(proId)=>{
+        return new Promise((resolve,reject)=>{
+            db.get().collection(collection.PRODUCT_COLLECTION).deleteOne({_id:ObjectId(proId)}).then((response)=>{
+                console.log(response);
+                resolve(response)
+            })
+        })
+
+    },
+    getProductDetails:(proId)=>{
+        return new Promise((resolve,reject)=>{
+            db.get().collection(collection.PRODUCT_COLLECTION).findOne({_id:objectId(proId)}).then((product)=>{
+                resolve(product)
+            })    
+        })
+    },
+    updateProduct:(proId,proDetails)=>{
+        return new Promise((resolve,reject)=>{
+            db.get().collection(collection.PRODUCT_COLLECTION)
+            .updateOne({_id:objectId(proId)},{
+                $set:{
+                    Name:proDetails.Name,
+                    Author:proDetails.Author,
+                    Price:proDetails.Price,
+                }
+            }).then((response)=>{
+                resolve()
+            })
+        })    
+
+    }
+}
